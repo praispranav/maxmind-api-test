eeb2fb8236892ea670ecb3501866e6d3f92dc1b0
praispranav <praispranav@gmail.com>
2023-08-22 22:45:53 +0530

    Test1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const username = '';
+  const password = '';
+  const getIpAddress = async () =>{
+    try{
+      const { data:response } = await axios.get(`https://ifconfig.me`);
+      console.log(response2);
+
+      const maxMindApi = `https://geoip.maxmind.com/geoip/v2.1/insights/${response2}`;
+      const headers = {
+        'Authorization': 'Basic ' + btoa(`${username}:${password}`).toString('base64'),
+        'Content-Type': 'application/vnd.maxmind.com-country+json; charset=UTF-8; version=2.1',
+        'Accept-Encoding': 'gzip, deflate'
+      };
+
+      const { data:response2 } = await axios.get(maxMindApi, {
+        headers,
+        responseType: 'json', 
+      });
+
+      console.log('Final Data', response2);
+
+      console.log("Response", response);
+
+    } catch(error){
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    getIpAddress()
+  },[])
   return (
     <div className="App">
       <header className="App-header">
